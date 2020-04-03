51a2c733dbd9c90079f1960efeb8bb3bc8111511
lwears <liamwears@fastmail.com>
2020-04-03 12:09:52 +0200

    updating cateogry filter function

--- screens/ArticleList.tsx
+++ screens/ArticleList.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
 import axios from 'axios';
 import { NavigationStackScreenComponent } from 'react-navigation-stack';
 import Card from '../components/card';
@@ -10,48 +10,59 @@ const ArticleList: NavigationStackScreenComponent = ({ navigation }) => {
   const [articles, setArticles] = useState<Article[]>([]);
   const [page, setPage] = useState<number>(1);
   const [category, setCategory] = useState<number>(undefined)
+  const [author, setAuthor] = useState<number>(undefined)
+  const [moreArticles, setMoreArticles] = useState<boolean>(true)
+  // let flatlistRef;
 
   const updateCategory = (id: number) => {
     if(id === category) {
+      // flatlistRef.scrollToIndex({animated: true, index: 0})
       setCategory(undefined);
       setPage(1);
+      setMoreArticles(true);
       setArticles([]);
     } else {
+      setPage(1);
+      setMoreArticles(true);
       setCategory(id);
+      setArticles([]);
     }
   }
   
   const updateArticles = async (pageNr: number) => {
-    const { data } = await axios.get(`http://localhost:8080/?page=${pageNr}`);
-    setArticles([...articles, ...data ]);
-    console.log(pageNr);
-  };
-
-  const fetchMoreArticles = async () => {
-    if ( category ) {
-      fetchMoreOnCategory(page);
-    } else {
-      updateArticles(page + 1);
-      setPage(page + 1);
+    try {
+      const { data } = await axios.get(`http://localhost:8080/?page=${pageNr}`);
+      setArticles([...articles, ...data ]);
+    } catch(err) {
+      setMoreArticles(false);
     }
   };
 
-  const fetchFirstOnCategory = async () => {
-    const { data } = await axios.get(`http://localhost:8080/?page=1&category=${category}`);
-    setArticles(data);
-    setPage(1);
+  const fetchMoreArticles = async () => {
+    if( moreArticles ) {
+      if ( category ) {
+        fetchMoreOnCategory(page + 1);
+        setPage(page + 1);
+      } else {
+        updateArticles(page + 1);
+        setPage(page + 1);
+      }
+    }
   };
 
   const fetchMoreOnCategory = async (pageNr: number) => {
-    const { data } = await axios.get(`http://localhost:8080/?page=${pageNr+1}&category=${category}`);
-    setArticles([...articles, ...data]);
-    setPage(page +1 );
+    try {
+      const { data } = await axios.get(`http://localhost:8080/?page=${pageNr}&category=${category}`);
+      setArticles([...articles, ...data]);
+    } catch(err) {
+      setMoreArticles(false);
+    }
   };
   
   useEffect(() => { updateArticles(1); }, []);
 
   useEffect(() => {
-    fetchFirstOnCategory();
+    fetchMoreOnCategory(1);
   }, [category]);
   
   if (articles.length === 0) {
@@ -66,17 +77,9 @@ const ArticleList: NavigationStackScreenComponent = ({ navigation }) => {
     <FlatList
       data={articles}
       renderItem={({item}: {item: Article}) => <Card article={item} navigation={navigation} updateCategory={updateCategory}/>}
-        
-        // if(!category) {
-        //   return <Card article={item} navigation={navigation} updateCategory={updateCategory}/> 
-        // } else {
-        //   const catIds = item.category.map(cat => cat.id);
-        //   if (catIds.includes(category)) {
-        //     return <Card article={item} navigation={navigation} updateCategory={updateCategory}/>
-        //   }
-        // }
       keyExtractor={(item: Article) => item.id.toString()}
       onEndReached={fetchMoreArticles}
+      // ref={ref => {flatlistRef = ref}}
       />
   );
 }
